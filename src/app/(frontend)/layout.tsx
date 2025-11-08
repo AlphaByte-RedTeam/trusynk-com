59e7bb5af1d5c7fada40d9941d4a4647b2f21662
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-08 23:11:16 +0700

    style(layout): add custom font and adjust width + height

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 import './globals.css'
+import { Inter, Playfair_Display } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+})
+
+const playfairDisplay = Playfair_Display({
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['serif'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+})
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -10,8 +26,11 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
-      <body>
+    <html
+      lang="en"
+      className={`${inter.className} ${playfairDisplay.className} h-screen min-w-screen`}
+    >
+      <body className="bg-blue-1">
         <main>{children}</main>
       </body>
     </html>
