e768ae0287e0704b6f9d0fd868e7491a450b85cd
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-15 23:27:17 +0700

    feat: implement header component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/hero'
 import './globals.css'
 import Footer from '@/components/footer'
+import Header from '@/components/header'
 
 export default async function HomePage() {
   return (
     <div className="flex flex-col min-h-screen w-screen items-center justify-center align-middle">
+      <Header />
       <Hero />
       <Footer />
     </div>
