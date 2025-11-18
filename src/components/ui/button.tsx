a043a15874317ba115bd58be732e0b0c19bc05e2
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-18 20:23:56 +0700

    style: change link text color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline: 'border border-brand-7 bg-brand-4 text-brand-12 shadow-xs hover:bg-brand-5',
         secondary: 'bg-brand-4 text-brand-11 hover:bg-brand-5',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 'text-white underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
