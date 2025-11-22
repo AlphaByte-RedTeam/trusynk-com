8a540b512ba3b613a2294e5150c4d68819b35ad5
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-22 09:11:49 +0700

    feat(collections): add early access form

---
+++ src/collections/EarlyAccessForm.ts
@@ -0,0 +1,9 @@
+import { CollectionConfig } from 'payload'
+
+export const EarlyAccessForm: CollectionConfig = {
+  slug: 'early-access-forms',
+  fields: [
+    { name: 'name', type: 'text', required: true, maxLength: 255, label: 'Name' },
+    { name: 'email', type: 'email', required: true, unique: true, label: 'Email' },
+  ],
+}
